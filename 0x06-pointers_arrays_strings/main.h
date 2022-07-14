f455f640af9fbb03da2828eea8ad8423bc2635bb
abenitade <abenitade1212@gmail.com>
2022-07-13 23:06:15 -0700

    task 0

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,23 +1,6 @@
 #ifndef MAIN_H
 
 #define MAIN_H
-
-
-
-/**
- *
- *  * File: holberton.h
- *
- *   * Auth: Brennan D Baraban
- *
- *    * Desc: Header file containing prototypes for all functions
- *
- *     *       used in the 0x05-pointers_arrays_strings directory.
- *
- *      */
-
-
-
 int _putchar(char c);
 
 char *_strcat(char *dest, char *src);
@@ -37,13 +20,3 @@ char *cap_string(char *);
 char *leet(char *);
 
 char *rot13(char *);
-
-void print_number(int n);
-
-char *infinite_add(char *n1, char *n2, char *r, int size_r);
-
-void print_buffer(char *b, int size);
-
-
-
-#endif
